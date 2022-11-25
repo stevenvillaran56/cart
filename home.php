37f7dbac923bb4b8b6ae55627ba1f9c4c05e76ec
stevenvillaran <stevenvillaran56@gmail.com>
2022-11-25 22:42:08 +0800

    Desigg

---
+++ home.php
@@ -0,0 +1,177 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link rel="stylesheet" href="ned.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<div class="container">
+    <br> <h3 class="h1">shopping Cart</h3> <br> <hr>
+    <div class="row">
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2">
+                    <a href="">
+                        <img class="pic-1" src="img/1a.png">
+                        <img class="pic-2" src="img/1b.png">
+                    </a>
+                    
+                    <a class="add-to-cart" href="">Add to cart</a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Nike Revolution 5</a></h3>
+                    <span class="badge bg-secondary">₱2,200.00</span>
+                    
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2">
+                    <a href="">
+                        <img class="pic-1" src="img/b1.png">
+                        <img class="pic-2" src="img/b2.png">
+                    </a>
+                    
+                    <a class="add-to-cart" href="">Add to cart</a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Black and White Nike</a></h3>
+                    <span class="badge bg-secondary">₱2,500.00</span>
+
+                    
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2">
+                    <a href="">
+                        <img class="pic-1" src="img/c2.png">
+                        <img class="pic-2" src="img/c1.png">
+                    </a>
+                    
+                    <a class="add-to-cart" href="">Add to cart</a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Blue White Shoe</a></h3>
+                    <span class="badge bg-secondary">₱1,200.00</span>
+
+                   
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2">
+                    <a href="">
+                        <img class="pic-1" src="img/d2.png">
+                        <img class="pic-2" src="img/d1.png">
+                    </a>
+                    
+                    <a class="add-to-cart" href="">Add to cart</a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Nike Go FlyEase</a></h3>
+                    <span class="badge bg-secondary">₱1,800.00</span>
+
+                    
+                </div>
+            </div>
+        </div>
+        
+        
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2">
+                    <a href="">
+                        <img class="pic-1" src="img/e1.png">
+                        <img class="pic-2" src="img/e2.png">
+                    </a>
+                    
+                    <a class="add-to-cart" href="">Add to cart</a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">All Black Shoe Nike</a></h3>
+                    <span class="badge bg-secondary">₱2,500.00</span>
+
+                    
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2">
+                    <a href="">
+                        <img class="pic-1" src="img/f2.png">
+                        <img class="pic-2" src="img/f1.png">
+                    </a>
+                    
+                    <a class="add-to-cart" href="">Add to cart</a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">White and Gray Shoe Nike</a></h3>
+                    <span class="badge bg-secondary">₱1,200.00</span>
+
+                    
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2">
+                    <a href="">
+                        <img class="pic-1" src="img/g1.png">
+                        <img class="pic-2" src="img/g2.png">
+                    </a>
+                    
+                    <a class="add-to-cart" href="">Add to cart</a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Air Max Nike</a></h3>
+                    <span class="badge bg-secondary">₱1,300.00</span>
+
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2">
+                    <a href="">
+                        <img class="pic-1" src="img/hh.png">
+                        <img class="pic-2" src="img/h.png">
+                    </a>
+                    
+                    <a class="add-to-cart" href="">Add to cart</a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Nike Shoe</a></h3>
+                    <span class="badge bg-secondary">₱1,900.00</span>
+
+                    
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</div>
+<hr>
+</body>
+</html>
